461b01b0a9c8ca68b680708f07f81c7954c3d127
C. Flores <flores@petalphile.com>
2011-11-11 19:34:23 -0800

    add more compounds, fix 5% bug, change target to 1.5%. need to add these to model...

--- build/glut.js
+++ build/glut.js
@@ -1,41 +1,33 @@
 (function() {
   var Calculate, Glut;
-  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
-
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
   Glut = (function() {
-
     __extends(Glut, Batman.App);
-
     function Glut() {
       Glut.__super__.constructor.apply(this, arguments);
     }
-
     Glut.global(true);
-
     return Glut;
-
   })();
-
   Calculate = (function() {
-
     __extends(Calculate, Batman.Model);
-
     function Calculate() {
       Calculate.__super__.constructor.apply(this, arguments);
     }
-
     Calculate.accessor('solute', function() {
-      return Math.round(this.get('dilution_volume') * 1.6 / this.get('source'));
+      return Math.round(this.get('dilution_volume') * 1.5 / this.get('source'));
     });
-
     Calculate.accessor('solvent', function() {
       return Math.round(this.get('dilution_volume') - this.get('solute'));
     });
-
     return Calculate;
-
   })();
-
   Glut.calculate = new Calculate;
-
 }).call(this);
--- glut.coffee
+++ glut.coffee
@@ -1,6 +1,6 @@
 class Glut extends Batman.App
   @global yes
 class Calculate extends Batman.Model
-  @accessor 'solute', -> Math.round( @get('dilution_volume') * 1.6 / @get('source') )
+  @accessor 'solute', -> Math.round( @get('dilution_volume') * 1.5 / @get('source') )
   @accessor 'solvent', -> Math.round( @get('dilution_volume') - @get('solute') )
 Glut.calculate = new Calculate
